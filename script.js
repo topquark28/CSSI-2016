28002040fbff4e9da5726a57db59c3e8b4712f16
Chris Reimer <reimerc@gmail.com>
2016-07-17 17:50:29 -0700

    JS cleanup

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+console.log("Hello, Chris!");
+
+function doAnimation() {
+  console.log("Ready set go!");
+  $('p').animate( {fontsize: 24}, 1500);
+}
+
+$(document).ready(doAnimation);
